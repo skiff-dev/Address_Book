cc2a2a7df79d1bf47831373da2055d6be1c398f0
AndriyMartynyuk <andriy.martynyuk.if@gmail.com>
2022-12-24 17:00:24 +0200

    HW10 Address Book

---
+++ address_book.py
@@ -0,0 +1,47 @@
+from collections import UserDict
+
+
+class AddressBook(UserDict):
+    """Class for creating user's addressbooks"""
+
+    def add_record(self, record):
+        self.data[record.name] = record.show_contact()
+
+    def remove_record(self, record):
+        self.data.pop(record.name, None)
+
+    def show_records(self):
+        return self.data
+
+
+class Record:
+    """Creating user's contacts"""
+
+    def __init__(self, name, phone=None):
+        self.name = name
+        if phone is None:
+            self.phone = []
+
+    def add_phone(self, phone):
+        self.phone.append(phone)
+
+    def change_phone(self, phone):
+        self.phones = phone
+
+    def delete_phone(self):
+        self.phones = []
+
+    def show_contact(self):
+        return {"name": self.name, "phone": self.phone}
+
+
+class Field:
+    pass
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    pass
